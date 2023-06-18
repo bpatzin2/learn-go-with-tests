1e8f82831b5aecf217385edc3ee3538e620bfa5a
Brian Patzin <bpatzin2@gmail.com>
2023-06-18 14:21:11 -0700

    hello world section complete

--- hello.go
+++ hello.go
@@ -1,17 +1,34 @@
 // hello world in go
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
+const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
-		return "Hello, World"
+		name = "World"
 	}
-	return englishHelloPrefix + name
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) string {
+	switch language {
+	case french:
+		return frenchHelloPrefix
+	case spanish:
+		return spanishHelloPrefix
+	default:
+		return englishHelloPrefix
+	}
 }
 
 func main() {
-	fmt.Println(Hello("Brian"))
+	fmt.Println(Hello("Brian", ""))
 }
